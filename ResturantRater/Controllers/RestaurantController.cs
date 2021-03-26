672c902d5159da4695b83eb8d05fb47012c04d50
Meaza <meazastesfaye@gmail.com>
2021-03-25 23:04:13 -0400

    Details and Edit added

--- ResturantRater/Controllers/RestaurantController.cs
+++ ResturantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -66,6 +67,55 @@ namespace ResturantRater.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
 
+        }
+
+        //Get:Restaurant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //Post: Restaurant/Edit{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit (Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
+        //Get:Restaurant/Details/{id}
+
+        public ActionResult Details (int? id) //We want to get an ID from the Index button,
+            //find a restaurant based on that ID, and then give that ID to a view
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
         }
     }
 }
